5d348ddabb07d57d7380edfa9052d4b06ab4c7f7
Vishnu B <vishnub1626@gmail.com>
2020-08-03 22:21:40 +0530

    Fixes the products migration

--- database/migrations/2020_08_03_144304_create_products_table.php
+++ database/migrations/2020_08_03_144304_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->float('price');
+            $table->double('price');
             $table->string('sku');
             $table->text('description');
             $table->timestamps();
